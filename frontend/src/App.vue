3cfa23ce4bb01cbf813cbdc10d1d5687a8c0b3ec
Paul Leflon <leflon.paul@gmail.com>
2024-12-17 22:46:53 +0800

    feat(frontend): add title and favicon

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,21 @@
 <script setup>
-import {RouterView} from 'vue-router';
+import {RouterView, useRouter} from 'vue-router';
 import MenuBar from './components/MenuBar.vue';
 import {onMounted} from 'vue';
 import api from './lib/api';
 import {store} from './lib/store';
-
+const router = useRouter();
 onMounted(async () => {
   const res = await api.private.get('/me');
   if (!res.error)
     store.customer = res.customer;
 });
 
+router.beforeEach((to, from) => {
+  const {title} = to.meta;
+  if (title)
+    document.title = title + ' • WuWu';
+});
 
 </script>
 
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,24 +7,36 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: {
+        title: 'Home'
+      }
     }, 
     {
       path: '/booking_process/:id',
       name: 'booking',
       component: () => import('../views/BookingProcessView.vue'),
-      props: true
+      props: true,
+      meta: {
+        title: 'Book a trip'
+      }
     },
     {
       path: '/booking/:id',
       name: 'View booking',
       component: () => import('../views/BookingView.vue'),
-      props: true
+      props: true, 
+      meta: {
+        title: 'My trip'
+      }
     },
     {
       path: '/my',
       name: 'My bookings',
-      component: () => import('../views/MyView.vue')
+      component: () => import('../views/MyView.vue'),
+      meta: {
+        title: 'My bookings'
+      }
     }
   ]
 })
